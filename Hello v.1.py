3aaf66b7f6ce733862ffadc837f34e33bd360a3b
EAS97 <Varfolomik@yandex.ru>
2025-05-25 16:57:11 +0300

    Hello!
    A simple Python program to test Github

---
+++ Hello v.1.py
@@ -0,0 +1,7 @@
+print("Hello!\nSay Hello back? (y/n)")
+hello = input()
+
+if hello == "y":
+    print("*You said Hello back. Good job!*")
+elif hello == "n":
+    print("*You refused to say Hello back. How rude...*")
