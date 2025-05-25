0f510fbb584dde4b3bdbf959b31360e6c1104c2f
EAS97 <Varfolomik@yandex.ru>
2025-05-25 17:04:49 +0300

    Hello v.1
    Updated Version.
    Fixed crash if answer in not y/n
    added exit timeout.

--- Hello v.1.py
+++ Hello v.1.py
@@ -1,7 +1,17 @@
+import sys
+import time
+
 print("Hello!\nSay Hello back? (y/n)")
 hello = input()
+
+while hello != "y" and hello != "n":
+    print("It is y or n.\nThere is no other choice...")
+    hello = input()
 
 if hello == "y":
     print("*You said Hello back. Good job!*")
 elif hello == "n":
     print("*You refused to say Hello back. How rude...*")
+    
+time.sleep (3)
+sys.exit()
